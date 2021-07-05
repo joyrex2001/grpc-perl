46f04f98720f57782eecca0323048a5fb3ee5ada
Graham TerMarsch <graham@howlingfrog.com>
2021-07-05 16:11:29 -0700

    Add a `MANIFEST` file, so that one can `make dist`.
    When using `ExtUtils::MakeMaker` to generate a `Makefile`, said
    `Makefile` has a `dist` target that will build your distribution tarball
    for you automatically (thus eliminating the need for a separate
    `release.sh` script).

--- release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-VERSION=`grep our\ \$VERSION lib/Grpc/XS.pm|sed "s/.*'\(.*\)';/\1/"`
-PACKAGE=grpc-xs-${VERSION}
-FILES=`git ls-files|grep -v Vagrantfile|grep -v .gitignore|grep -v release.sh`
-
-## gnu tar only...
-gtar cvzf ${PACKAGE}.tar.gz ${FILES} --transform s,^,${PACKAGE}/,
